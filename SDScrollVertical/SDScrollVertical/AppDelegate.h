020e21765b76d244ef8a8530fffefd6849a54fd2
苏亚 <792708835@qq.com>
2016-01-15 14:14:02 +0800

    new

---
+++ SDScrollVertical/SDScrollVertical/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDScrollVertical
+//
+//  Created by Mac on 16/1/15.
+//  Copyright © 2016年 suya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDScrollVertical/SDScrollVertical/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDScrollVertical
+//
+//  Created by Mac on 16/1/15.
+//  Copyright © 2016年 suya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
